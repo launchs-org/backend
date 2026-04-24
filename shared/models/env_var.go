6a394f2da0389fe3710b85b7a473804813da03e8
mattuu0 <tomatomato170@gmail.com>
2026-04-25 01:24:15 +0900

    一度作り直すために削除

--- shared/models/env_var.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// EnvVar Deployment に紐づく環境変数
-type EnvVar struct {
-	ID           string `gorm:"primaryKey" json:"id"`
-	DeploymentID string `gorm:"index" json:"deployment_id"`
-	Key          string `json:"key"`
-	Value        string `json:"value"` // 平文保存 or 暗号化
-}
