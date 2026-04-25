67fe740d3b9996c2f739cdb69980f9bde79a3816
mattuu0 <tomatomato170@gmail.com>
2026-04-25 19:10:55 +0900

    feat: データベースをMySQLに変更し、Kubernetesクライアントの依存関係を追加

--- backend/src/database/database.go
+++ backend/src/database/database.go
@@ -1,10 +1,9 @@
 package database
 
 import (
-	"fmt"
 	"os"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -12,17 +11,10 @@ var DB *gorm.DB
 
 // Init はデータベース接続を初期化します
 func Init() {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+	dsn := os.Getenv("DATABASE_DSN")
 
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database: " + err.Error())
 	}
